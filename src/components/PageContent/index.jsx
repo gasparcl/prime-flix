1603a194db9423db7270a85002e3acc09139c671
Lucas Gaspar de Carvalho <lucas@ecosis.com.br>
2022-08-19 17:57:26 -0300

    Att aqvs

--- src/components/PageContent/index.jsx
+++ src/components/PageContent/index.jsx
@@ -1,6 +1,13 @@
+import { Paper } from "@material-ui/core";
 import React from "react";
 import { ContentContainer } from "./styles";
 
 export default function PageContent({ children, ...props }) {
-	return <ContentContainer className="pt-6">{children}</ContentContainer>;
+    return (
+        <>
+            <Paper>
+                <ContentContainer>{children}</ContentContainer>
+            </Paper>
+        </>
+    );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,12 @@ import api from "../../services/api";
 
 import { Grid, Typography } from "@material-ui/core";
 import { MoviesGrid } from "./styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
 
 export default function Home() {
     const [filmes, setFilmes] = useState([]);
@@ -31,9 +37,44 @@ export default function Home() {
                 {filmes.map((filme) => {
                     return (
                         <Grid item xs={6} key={filme.id}>
-                            <Typography variant="h5" color="primary">
-                                {filme.title}
-                            </Typography>
+                            <Card>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="Contemplative Reptile"
+                                        height="140"
+                                        image=""
+                                        title="Contemplative Reptile"
+                                    />
+                                    <CardContent>
+                                        <Typography
+                                            gutterBottom
+                                            variant="h5"
+                                            component="h2"
+                                        >
+                                            Lizard
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            color="textSecondary"
+                                            component="p"
+                                        >
+                                            Lizards are a widespread group of
+                                            squamate reptiles, with over 6,000
+                                            species, ranging across all
+                                            continents except Antarctica
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        Share
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                        Learn More
+                                    </Button>
+                                </CardActions>
+                            </Card>
                         </Grid>
                     );
                 })}
