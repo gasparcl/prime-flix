ccc300f1eae5285b6280e6b9b1910201461e4da9
João Alves <joao.alves@ecosis.com.br>
2022-10-21 10:56:04 -0300

    Feat: user moviedb api

---
+++ mobile/src/config/themoviedb.ts
@@ -0,0 +1,9 @@
+import {REACT_APP_THEMOVIEDB_API_KEY} from "@env"
+
+export const THEMOVIEDB_CONFIG = {
+    api_key: REACT_APP_THEMOVIEDB_API_KEY,
+    language: "en-us",
+    // region: "BRA",
+}
+
+export const THEMOVIEDB_BANNER_URL = "https://image.tmdb.org/t/p/original"
